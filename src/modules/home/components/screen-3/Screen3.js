7ae1cee69893d243c799e7c4e1d22f07ffc63768
dinesh <ddinesh.36@gmail.com>
2022-01-13 14:55:44 +0530

    Changes for setting the screen3 value.

--- src/modules/home/components/screen-3/Screen3.js
+++ src/modules/home/components/screen-3/Screen3.js
@@ -1,42 +1,51 @@
 import Header from "../../../layout/header/Header";
 import Footer from "../../../layout/footer/Footer";
 import SideText from "../../../layout/side-text/SideText";
-import Header1 from "../../../shared/header1/Header1";
+import Header1 from "../../../shared/components/header1/Header1";
 import './Screen3.css';
-import {useCallback, useEffect, useRef} from "react";
+import {useRef} from "react";
+import useOnScreen from "../../../shared/hooks/useOnScreen";
 
 export default function Screen3(){
-    const onWindowScroll = useCallback((event)=>{
-        console.log('Scroll Event', event);
-    },[]);
+    const firstScrollContentRef = useRef();
+    const secondScrollContentRef = useRef();
+    const thirdScrollContentRef = useRef();
 
-    useEffect(()=>{
-        window.addEventListener('scroll', onWindowScroll);
-        return ()=>{window.removeEventListener('scroll', onWindowScroll)}
-    },[]);
+    const isThirdScrollIntersected = useOnScreen(thirdScrollContentRef);
+    const isSecondScrollIntersected = useOnScreen(secondScrollContentRef);
+    const isFirstScrollIntersected = useOnScreen(firstScrollContentRef);
+
+    const showFirstTextContent = isFirstScrollIntersected && !isSecondScrollIntersected && !isThirdScrollIntersected;
+    const showSecondTextContent = isSecondScrollIntersected && !isThirdScrollIntersected;
+    const showThirdTextContent = isThirdScrollIntersected;
 
     return <div className={'screen3'}>
         <Header />
         <SideText />
         <div className={'text-container'}>
             <div>
-                <Header1 className={'text-content1'}>
+                {showFirstTextContent && <Header1 className={`text-content1 text-open-animation`}>
                     Be weird, dumb, sexy, broken, whatever. Just not f&$!#g boring.
-                </Header1>
-                <Header1 className={'text-content2'}>
+                </Header1>}
+                {showSecondTextContent && <Header1 className={'text-content2 text-open-animation'}>
                     Second Content
-                </Header1>
-                <Header1 className={'text-content3'}>
+                </Header1>}
+                {showThirdTextContent && <Header1 className={'text-content3 text-open-animation'}>
                     Third Content
-                </Header1>
+                </Header1>}
             </div>
-            <div className={'image-modal'}>
+            <div className={'image-modal' +
+            `${showFirstTextContent ? ' first-part': ''}`+
+            `${showSecondTextContent ? ' second-part': ''}`+
+            `${showThirdTextContent ? ' third-part': ''}`}>
+                <div className={'mash'} />
+                <div className={'actor'} />
             </div>
         </div>
-        <div className={'scroll-contents'}>
-            <div className={'scroll-content1'} />
-            <div className={'scroll-content2'} />
-            <div className={'scroll-content3'} />
+        <div className={`scroll-contents`}>
+            <div ref={firstScrollContentRef} className={'scroll-content1'} />
+            <div ref={secondScrollContentRef} className={'scroll-content2'} />
+            <div ref={thirdScrollContentRef} className={'scroll-content3'} />
         </div>
         <Footer />
     </div>
