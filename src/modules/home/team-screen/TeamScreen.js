bddb184ea1357f667aee293e9971212f5bde053a
dinesh <ddinesh.36@gmail.com>
2022-02-14 13:36:20 +0530

    Changes for setting the assets for the teams screen.

--- src/modules/home/team-screen/TeamScreen.js
+++ src/modules/home/team-screen/TeamScreen.js
@@ -1,22 +1,13 @@
 import "./TeamScreen.css";
 import useWindowOnScrollRatio from "../../../shared/hooks/useOnWindowScrollRatio";
 import titleIcon from '../../../assets/images/team-screen/title-icon.svg';
-import teamUser1 from '../../../assets/images/team-screen/team-user-1.png';
-import teamUser2 from '../../../assets/images/team-screen/team-user-2.png';
 import Header1 from "../../../shared/components/header1/Header1";
 import Header3 from "../../../shared/components/header3/Header3";
 import useWindowDimension from "../../../shared/hooks/useOnWindowDimension";
 import {useMemo} from "react";
+import {teamListTemp} from "./TeamScreen.data";
 
-const teamListTemp = [
-    {title: 'Team Member', title2: '/// Job Role', description: 'Lorem ipsum  sit amet, consectetur or sit amet, consectetur', imgSrc: teamUser1},
-    {title: 'Team Member', title2: '/// Job Role', description: 'Lorem ipsum  sit amet, consectetur or sit amet, consectetur', imgSrc: teamUser2},
-    {title: 'Team Member', title2: '/// Job Role', description: 'Lorem ipsum  sit amet, consectetur or sit amet, consectetur', imgSrc: teamUser1},
-    {title: 'Team Member', title2: '/// Job Role', description: 'Lorem ipsum  sit amet, consectetur or sit amet, consectetur', imgSrc: teamUser2},
-    {title: 'Team Member', title2: '/// Job Role', description: 'Lorem ipsum  sit amet, consectetur or sit amet, consectetur', imgSrc: teamUser2},
-];
-
-const teamList = [...teamListTemp, ...teamListTemp, ...teamListTemp, ...teamListTemp];
+const teamList = [...teamListTemp];
 
 export default function TeamScreen({scrollContainerHeight}) {
     const scrollRatio = useWindowOnScrollRatio({
@@ -49,14 +40,14 @@ export default function TeamScreen({scrollContainerHeight}) {
                 return <div className={'team-row '} key={index}
                             style={{paddingLeft: leftPadding, ...rowStyle}}>
                     {teamList.slice(index*membersInRow, (index*membersInRow + membersInRow ) )
-                        .map(({title, title2, description, imgSrc}, innerIndex) => {
+                        .map(({title, title2, description, imageName}, innerIndex) => {
                             return <div className={'team-member'} key={innerIndex}>
                                 <div className={'text-container'}>
                                     <Header3 className={'title'}> {title}</Header3>
                                     <Header3 className={'title'}> {title2}</Header3><br/>
                                     <div className={'description'}> {description}</div>
                                 </div>
-                                <img src={imgSrc}/>
+                                <img src={`/images/team-people/${imageName}.png`}/>
                             </div>
                         })}
                 </div>
