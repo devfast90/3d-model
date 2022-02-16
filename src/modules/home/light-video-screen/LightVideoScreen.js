3ff14e04bec6f74f61996ea81f2a5a08544d87fc
dinesh <ddinesh.36@gmail.com>
2022-02-16 17:18:40 +0530

    Changes for setting the light video screen changes.

--- src/modules/home/light-video-screen/LightVideoScreen.js
+++ src/modules/home/light-video-screen/LightVideoScreen.js
@@ -2,9 +2,7 @@ import "./LightVideoScreen.scss";
 import useWindowOnScrollRatio from "../../../shared/hooks/useOnWindowScrollRatio";
 import Header1 from "../../../shared/components/header1/Header1";
 import Header3 from "../../../shared/components/header3/Header3";
-import {useEffect, useLayoutEffect, useState} from "react";
-import useOnContentScroll from "../../../shared/hooks/useOnContentScroll";
-import useWindowDimension from "../../../shared/hooks/useOnWindowDimension";
+import {useEffect, useRef, useState} from "react";
 import {FONT_COLORS} from "../scroll-container/ScrollContainer";
 
 export default function LightVideoScreen({scrollContainerHeight, setFontColor}) {
@@ -33,24 +31,43 @@ export default function LightVideoScreen({scrollContainerHeight, setFontColor}) 
                         muted
                     ></video>
                 </div>
-                <div className={'side-text-content'}>
-                    {scrollRatio >= 0 && <Header1 className={"actor-text-content text-open-animation"}>
-                        Who do you want to be?
-                    </Header1>}
-                    {(scrollRatio >= 0 && scrollRatio < 0.33) &&
-                    <Header3 className={"actor-text-content text-open-animation"}>
-                        Six avatar<br/>archetypes.<br/>
-                    </Header3>}
-                    {(scrollRatio >= 0.33 && scrollRatio <= 0.66) &&
-                    <Header3 className={"actor-text-content text-open-animation"}>
-                        <br/>10k Bad Influencers.<br/>
-                    </Header3>}
-                    {scrollRatio > 0.66 && <Header3 className={"actor-text-content text-open-animation"}>
-                        All want to be<br/>‘Metaverse
-                        famous’
-                    </Header3>}
-                </div>
+                {scrollRatio > 0 && <SideTextContent scrollRatio={scrollRatio}/>}
             </div>
         </div>
     );
+}
+
+const texts = [
+    {content: <>Six avatar<br/>archetypes.<br/></>},
+    {content: <>10k Bad Influencers.</>},
+    {content: <>All want to be<br/>‘Metaverse famous’</>}
+];
+
+const INITIAL_ACTIVE_TEXT_INDEX = 0;
+const TEXT_CHANGE_INTERVAL = 3000;
+const SideTextContent = ({scrollRatio})=>{
+    const [activeTextIndex, setActiveTextIndex] = useState(INITIAL_ACTIVE_TEXT_INDEX);
+    const activeTextIndexRef = useRef(INITIAL_ACTIVE_TEXT_INDEX);
+    const intervalRef = useRef(INITIAL_ACTIVE_TEXT_INDEX);
+
+    useEffect(()=>{
+        intervalRef.current = setInterval(()=>{
+            const newIndex = activeTextIndexRef.current + 1 >= texts.length ? 0 : activeTextIndexRef.current + 1
+            setActiveTextIndex(newIndex);
+            activeTextIndexRef.current = newIndex;
+        }, TEXT_CHANGE_INTERVAL);
+
+        return ()=>clearInterval(intervalRef.current);
+    }, []);
+
+    return <div className={'side-text-content'}>
+        {scrollRatio >= 0 && <Header1 className={"actor-text-content text-open-animation"}>
+            Who do you want to be?
+        </Header1>}
+        {texts.map(({content}, index) => {
+            return activeTextIndex === index && <Header3 className={"actor-text-content text-open-animation"}>
+                {content}
+            </Header3>
+        })}
+    </div>
 }
